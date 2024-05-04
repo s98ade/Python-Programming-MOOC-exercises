587e871bbb7620122dfdecfad8a6f7a99f4692ba
Sofia Ade <140052187+s98ade@users.noreply.github.com>
2024-05-04 12:44:26 +0300

    Add files via upload

--- spellchecker/spellchecker.py
+++ spellchecker/spellchecker.py
@@ -4,6 +4,8 @@
 #    words = len(file.readlines())
 #    print(words)
 
+import difflib
+
 def load_dictionary(file_path):
     with open(file_path, 'r') as file:
         words = set(file.read().split())
@@ -13,22 +15,34 @@ def spell_check(text, dictionary):
     words = text.split()
 
     corrected_text = []
+    suggestions = {}
+
     for word in words:
         if word.lower() in dictionary:
             corrected_text.append(word)
         else:
-            corrected_text.append(f"*{word}*")
+            suggestions[word] = difflib.get_close_matches(word, dictionary)
+
+            if suggestions[word]:
+                corrected_text.append(f"*{word}*")
+            else:
+                corrected_text.append(word)
 
     corrected_text = ' '.join(corrected_text)
-    return corrected_text
+    return corrected_text, suggestions
 
 def main():
     dictionary_file = 'wordlist.txt'
     dictionary = load_dictionary(dictionary_file)
 
-    text = input("Write text: ")
+    text = input("write text: ")
 
-    corrected_text = spell_check(text, dictionary)
+    corrected_text, suggestions = spell_check(text, dictionary)
     print(corrected_text)
+
+    print("suggestions:")
+    for misspelled_word, suggestion_list in suggestions.items():
+        if suggestion_list:
+            print(f"{misspelled_word}: {', '.join(suggestion_list)}")
 
 main()
